3f18bd472d073aa5a7af322e47cb3e8a3bc4d311
AnyLine <859882067@qq.com>
2021-06-05 10:18:14 +0800

    Update v4mb.sh

--- v4mb.sh
+++ v4mb.sh
@@ -7,9 +7,9 @@ cp v4mb/20-jup /etc/cont-init.d/20-jup
 cp v4mb/jshare.sh /jd/jshare.sh
 cp v4mb/config/diy.sh config/diy.sh
 cp v4mb/config/auth.json config/auth.json
-rm -rf v4mb/
+rm -rf v4mb
 cd panel
 pm2 start server.js
 ln -s /jd/config/v4mb/25-server /etc/cont-init.d/25-server
 echo -e "请访问 http://<ip>:5678，如果你做了映射，请使用实际映射的端口进行访问。
-默认用户名admin，密码adminadmin。
+默认用户名admin，密码adminadmin。"
