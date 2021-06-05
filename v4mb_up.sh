d41c01f45a27ccc681e88859e54b6f7a150da52d
AnyLine <859882067@qq.com>
2021-06-06 07:28:07 +0800

    Update and rename v4mb_old.sh to v4mb_up.sh

--- v4mb_up.sh
+++ v4mb_up.sh
@@ -1,4 +1,4 @@
-wget -c https://raw.githubusercontent.com/Anylines/jd_v4_bot/main/v4mb_old.tar.gz
+wget -c https://raw.githubusercontent.com/Anylines/jd_v4_bot/main/v4mb_up.tar.gz
 tar -zxvf v4mb.tar.gz
 rm -f v4mb.tar.gz
 cp -r v4mb config/
