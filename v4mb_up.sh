62293410723ded6c59dca35692cd5a6f10ec2da5
AnyLine <859882067@qq.com>
2021-06-08 15:12:15 +0800

    Update v4mb_up.sh

--- v4mb_up.sh
+++ v4mb_up.sh
@@ -6,12 +6,15 @@ cp -r v4mb/panel panel
 cp v4mb/20-jup /etc/cont-init.d/20-jup
 cp v4mb/jshare.sh /jd/jshare.sh
 cp v4mb/config/diy.sh config/diy.sh
+cp v4mb/config/shortcut.list config/shortcut.list
+cp v4mb/config/shortcut.list config/shortcut.list
 rm -rf v4mb
 cd panel
 pm2 start server.js
 ln -s /jd/config/v4mb/25-server /etc/cont-init.d/25-server
-wget -c https://raw.githubusercontent.com/chiupam/JD_Diy/main/jbot/bot.py  /jd/jbot/diy/bot.py
-wget -c https://raw.githubusercontent.com/chiupam/JD_Diy/main/jbot/user.py  /jd/jbot/diy/user.py
+cd /jd/jbot/diy
+wget -c https://raw.githubusercontent.com/chiupam/JD_Diy/main/jbot/bot.py
+wget -c https://raw.githubusercontent.com/chiupam/JD_Diy/main/jbot/user.py
 pm2 stop jbot
 pm2 start jbot
 echo -e "面板和jbot已更新完成，请使用旧密码进行访问面板"
